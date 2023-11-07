26175b19c5ff899d9cc03d71aee6fc3fbfffa56e
kathleenforgiarini <kath.for199@gmail.com>
2023-11-06 21:08:39 -0500

    creating dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,32 @@
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import "./Dashboard.css";
+import { FaPlus, FaSearch } from "react-icons/fa";
 
 function Dashboard() {
   return (
-    <div className="">
+    <div>
       <NavBar />
-      <h3>Dashboard Page</h3>
+      <section id="dashboard">
+        <div id="headerLists">
+          <button type="button" className="newList">
+            New list <FaPlus className="plus" />
+          </button>
+          <div className="searchLists">
+            <select name="" id="">
+              <option value="All categories">All categories</option>
+            </select>
+            <input type="text" placeholder="Search" />
+            <button type="button">
+              <FaSearch />
+            </button>
+          </div>
+          <p className="totalLists">$8000</p>
+        </div>
+        <div id="lists">
+          <h1>My Lists</h1>
+        </div>
+      </section>
       <Footer />
     </div>
   );
