5865b2a76cb24aa90483aa4a5541d805a40dbff5
kathleenforgiarini <kath.for199@gmail.com>
2023-11-08 13:01:32 -0500

    adding modal to add new list

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,29 @@
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import "./Dashboard.css";
-import { FaPlus, FaSearch } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
 
 function Dashboard() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleNewListClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <NavBar />
       <section id="dashboard">
         <div id="headerLists">
-          <button type="button" className="newList">
+          <button
+            type="button"
+            className="newList"
+            onClick={handleNewListClick}
+          >
             New list <FaPlus className="plus" />
           </button>
           <div className="searchLists">
@@ -17,9 +31,6 @@ function Dashboard() {
               <option value="All categories">All categories</option>
             </select>
             <input type="text" placeholder="Search" />
-            <button type="button">
-              <FaSearch />
-            </button>
           </div>
           <p className="totalLists">$8000</p>
         </div>
@@ -27,6 +38,25 @@ function Dashboard() {
           <h1>My Lists</h1>
         </div>
       </section>
+
+      {isModalOpen && (
+        <div className="modalOverlay">
+          <div className="modalContent">
+            <h1>New list</h1>
+            <input type="text" placeholder="Nome" />
+            <textarea type="text" placeholder="Descrição"></textarea>
+            <select name="" id="">
+              <option value="Category">Category</option>
+            </select>
+            <input type="number" placeholder="Maximum price" />
+            <div className="buttons">
+              <button onClick={handleModalClose}>Save</button>
+              <button onClick={handleModalClose}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <Footer />
     </div>
   );
